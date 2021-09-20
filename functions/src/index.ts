2b84c8cf5675a9d563a5c23b20feed864576befd
Wongoose <wongoose.developer@gmail.com>
2021-09-20 18:47:03 +0800

    Deployed to Firebase works!!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,7 @@ import os = require("os");
 import fs = require("fs");
 
 exports.inputXeroApi = functions.https.onRequest((request, response) => {
+  // inputXeroApi2 | this function should be called by WebHooks, parsing in the csvFile - POST
   if (request.method !== "POST") {
     return response.status(405).end();
   }
@@ -14,15 +15,19 @@ exports.inputXeroApi = functions.https.onRequest((request, response) => {
   const tmpdir = os.tmpdir();
 
   console.log("Busboy init in functions!");
+  functions.logger.info("Busboy init functions with tempDIR: " + tmpdir);
   // This object will accumulate all the uploaded files, keyed by their name.
-  const uploads: any = {};
-  const fileWrites: any[] = [];
+  const uploads: any = {}; // Map<string, string>
+  const fileWrites: any[] = []; // Promise<void>[]
   // This code will process each file uploaded.
   busboy.on("file", (fieldname, file, filename) => {
     // Note: os.tmpdir() points to an in-memory file system on GCF
     // Thus, any files in it must fit in the instance"s memory.
     console.log(`Processed file ${filename}`);
+    console.log(`Processed fieldName ${fieldname}`);
     const filepath = path.join(tmpdir, filename);
+    console.log("File path is: " + filepath);
+    functions.logger.info("Filepath is: " + filepath);
     uploads[fieldname] = filepath;
 
     const writeStream = fs.createWriteStream(filepath);
@@ -51,7 +56,8 @@ exports.inputXeroApi = functions.https.onRequest((request, response) => {
   busboy.on("finish", async () => {
     await Promise.all(fileWrites);
     console.log("Busboy FINISH! Process saved files here");
-    console.log(uploads["testData"]);
+    console.log("Uploads is: " + uploads);
+    console.log(uploads["testData.csv"]);
 
     const tempFilePath = uploads["testData"];
     // Function to read csv which returns a promise so you can do async / await.
@@ -72,15 +78,17 @@ exports.inputXeroApi = functions.https.onRequest((request, response) => {
         }
       });
     };
-
+    // just using functions for now
     const convertToJson = async () => {
       const parsedData: any = await readCSV(tempFilePath);
+      // shows bankTransaction 1
       console.log("parsedData is index 0: \n" + parsedData);
       console.log(parsedData[0]);
+      // shows bankTransaction 2
       console.log("parsedData is index 1: \n" + parsedData);
       console.log(parsedData[1]);
     };
-
+    // callFunction
     convertToJson();
 
     for (const file in uploads) {
@@ -90,4 +98,5 @@ exports.inputXeroApi = functions.https.onRequest((request, response) => {
   });
 
   busboy.end(request.rawBody);
+  // END
 });
